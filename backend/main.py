eb0abea0b5de43d072cc365effec79c835a4233a
= <=>
2026-01-02 18:48:05 +0530

    Done POST method to store buses related data in buses route

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
-from database import create_buses_table
+from fastapi import FastAPI, Depends
+from database import create_buses_table, get_connection
+from models import CreateBuses
+from sqlite3 import Connection
 
 app = FastAPI()
 
 @app.on_event("startup")
 def startup():
     create_buses_table()
+    
+@app.post("/buses")
+def add_buses(bus: CreateBuses, db: Connection = Depends(get_connection)):
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO buses (bus_no, bus_type, start_bus, end_bus) VALUES (?, ?, ?, ?)",(bus.bus_no, bus.bus_type, bus.start_bus, bus.end_bus))
+    db.commit()
+    db.close()
+    return {"message": "Bus added successfully"}
