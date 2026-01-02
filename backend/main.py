3969508f8bfdc9fdb6a0ec7c580f5eb6b6d701dd
= <=>
2026-01-02 19:09:52 +0530

    Done GET method to fetch all buses details

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Depends
 from database import create_buses_table, get_connection
-from models import CreateBuses
+from models import CreateBuses, Buses
 from sqlite3 import Connection
+from typing import List
 
 app = FastAPI()
 
@@ -16,3 +17,12 @@ def add_buses(bus: CreateBuses, db: Connection = Depends(get_connection)):
     db.commit()
     db.close()
     return {"message": "Bus added successfully"}
+
+@app.get("/buses", response_model=List[Buses])
+def get_all_buses(db: Connection = Depends(get_connection)):
+    cursor = db.cursor()
+    
+    rows = cursor.execute("SELECT * FROM buses").fetchall()
+    cursor.close()
+    db.close()
+    return [dict(row) for row in rows]
