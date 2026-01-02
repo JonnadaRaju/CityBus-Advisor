1dc86b4bfd50550b64cff4276e10f75eee27e8e6
= <=>
2026-01-02 19:08:57 +0530

    Added Buses class for validation

--- backend/models.py
+++ backend/models.py
@@ -5,3 +5,10 @@ class CreateBuses(BaseModel):
     bus_type: str
     start_bus: str
     end_bus: str
+    
+class Buses(BaseModel):
+    bus_id: int
+    bus_no: str
+    bus_type: str
+    start_bus: str
+    end_bus: str
